fb07ba56bdc11819ccb10475f560f8b88d904788
HarisMalik123 <k220505@nu.edu.pk>
2025-01-30 21:20:31 +0500

    first commit

---
+++ config/databaseConnection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+const connectdb=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Mongodb is succesfully connected');
+    }
+    catch(error)
+    {
+        console.error(error.message);
+        throw new Error('Error While Connecting to database')
+        process.exit(1);
+    }
+}
