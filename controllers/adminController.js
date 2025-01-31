d5b4ebdf9e49e32eeb7909c35757940958686c7f
HarisMalik123 <k220505@nu.edu.pk>
2025-02-01 04:43:26 +0500

    Completed

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,14 +8,14 @@ export const markAttendance = async (req, res) => {
     const updatePromises = teams.map(async (team) => {
       const updatedEvent = await Event.findOneAndUpdate(
         { "registeredTeams.team_code": team.team_code },
-        { $set: { "registeredTeams.$.is_present": team.present } }, 
+        { $set: { "registeredTeams.$.is_present": team.present } },
         { new: true }
       );
       if (!updatedEvent) {
         notUpdatedTeams.push(team.team_name);
       }
     });
-    await Promise.all(updatePromises); 
+    await Promise.all(updatePromises);
     if (teams.length === notUpdatedTeams.length) {
       return res
         .status(404)
@@ -25,12 +25,10 @@ export const markAttendance = async (req, res) => {
         .status(201)
         .send({ message: "Attendance marked successfully" });
     } else {
-      return res
-        .status(201)
-        .send({
-          notUpdatedTeams,
-          message: "These teams were not marked present",
-        });
+      return res.status(201).send({
+        notUpdatedTeams,
+        message: "These teams were not marked present",
+      });
     }
   } catch (error) {
     console.error(error);
@@ -49,4 +47,30 @@ export const getTeams = async (req, res) => {
     return res.status(500).send({ message: "Internal Server Error" });
   }
 };
-export const getEvents=
+export const getEvents = async (req, res) => {
+  try {
+    const Events = await Event.find(
+      {},
+      { title: 1, department: 1, eventStartTime: 1, eventEndTime: 1 }
+    );
+    return res.status(201).json(Events);
+  } catch (error) {
+    return res.status(500).send({ message: "Internal Server Error" });
+  }
+};
+export const updateEventTime = async (req, res) => {
+  const { eventId, eventStartTime, eventEndTime } = req.body;
+  try {
+    const updatedEvent = await Event.findOneAndUpdate(
+      { _id: eventId },
+      { $set: { eventStartTime: eventStartTime, eventEndTime: eventEndTime } },
+      { new: true }
+    );
+    if(!updatedEvent){
+        return res.status(404).send({message:"Teams Not found"});
+    }
+    return res.json(updatedEvent);
+  } catch (error) {
+    return res.status(500).send({ message: "Internal Server Error" });
+  }
+};
