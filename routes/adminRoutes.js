662ef7c1970307a03463d3601211fab59cf80ec4
HarisMalik123 <k220505@nu.edu.pk>
2025-02-01 05:02:50 +0500

    Last Commit updated routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
 const router=express.Router();
+import * as eventServies from '../controllers/adminController.js'
 
-
-router.get('/',(req,res)=>{
-    res.send('Hi Admin');
-})
+router.get('/',eventServies.getAllTeams);
+router.patch('/updatetime',eventServies.updateEventTime);
+router.get('/events',eventServies.getEvents);
+router.get('/team/:teamcode',eventServies.getTeams);
+router.post('/attendance',eventServies.markAttendance);
 export default router;
