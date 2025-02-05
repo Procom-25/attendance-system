d453f67e1b650bf0f2c02605639081318ec18bae
HarisMalik123 <k220505@nu.edu.pk>
2025-02-05 22:00:17 +0500

    Backend Completed

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/user',userRoutes);
 app.use('/admin',adminRoutes);
-app.get('/',(req,res)=>{
-    res.send('Hi ');
-})
+
 
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,5 +2,5 @@ import express from 'express'
 import { verify } from '../controllers/userController.js';
 const router=express.Router();
 
-router.post('/verify/:teamcode',verify)
+router.post('/verify/',verify)
 export default router;
